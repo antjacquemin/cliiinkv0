52502c2012f94b96740457b31cacb8c08e83de05
Anthony JACQUEMIN <anthonyjacquemin@hotmail.fr>
2020-10-15 21:37:34 +0200

    Rectification BDD

--- bdd/cliiink.sql
+++ bdd/cliiink.sql
@@ -157,7 +157,7 @@ DROP TABLE IF EXISTS `marque`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `marque` (
   `id` smallint unsigned NOT NULL AUTO_INCREMENT,
-  `marque` varchar(15) COLLATE utf8_bin NOT NULL,
+  `nom` varchar(15) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -232,4 +232,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-14 23:01:11
+-- Dump completed on 2020-10-15 21:35:00
